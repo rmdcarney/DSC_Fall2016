72c03d063f80fa0ed29fcad4c91281d4108e23a1
rcarney <rmdcarney@gmail.com>
2016-11-28 10:34:15 +0100

    WORKING clk_divider. There was a logical error in the process - RTL schematic really helps with debugging.

--- lab4/stopwatch.srcs/src/clkDivider_10MHz_100Hz.vhd
+++ lab4/stopwatch.srcs/src/clkDivider_10MHz_100Hz.vhd
@@ -26,16 +26,17 @@ architecture behaviour of clk_divider is
 
 	-- Signals
 	constant counter_nbits	: integer := 20;
-	signal clk_in, clk_out	: STD_LOGIC := '0';
 	signal value			: STD_LOGIC_VECTOR( counter_nbits-1 downto 0 ) := ( others => '0' );
 	signal clkBuffer_I		: STD_LOGIC := '0';
 	signal clkBuffer_O		: STD_LOGIC := '0';
 
 	-- nbit counter for clk division
-	component nbit_counter is
+	component sync_nbitCounter is
 		generic( nbits	: integer := 8 ); -- Variable num of bits
 		port(
 				clk		: in STD_LOGIC;
+				reset   : in STD_LOGIC;
+				enable  : in STD_LOGIC;
 				value	: out STD_LOGIC_VECTOR( (nbits-1) downto 0 )
 			);
 	end component;
@@ -54,21 +55,21 @@ begin
 	clk_100Hz <= clkBuffer_O;
 
 	--Instantiate sub entities
-	counter : nbit_counter
+	counter : sync_nbitCounter
 		generic map( nbits => counter_nbits )
-		port map( clk => clk_100MHz, value => value );
+		port map( clk => clk_100MHz, value => value, reset => '0', enable => '1'  );
 
 	clk_buffer : bufg
 		port map( I => clkBuffer_I, O => clkBuffer_O);
 
 	-- Divide the clock
-	process( clk_100MHz )
+	process( clk_100MHz, value )
 	begin
 		if rising_edge( clk_100MHz ) then
-			if value = "1111010000100100000" then
-				clkBuffer_I <= '0';
+			if value < "0111101000010010000" then
+				clkBuffer_I <= '1';
 			else
-				clkBuffer_O <= '1';
+				clkBuffer_I <= '0';
 			end if;
 		end if;
 	end process;
